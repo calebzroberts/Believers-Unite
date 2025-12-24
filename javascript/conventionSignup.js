527c33a18f9bfa27f22f0fe427f9667d4ae20700
calebzroberts <caleb.roberts798@gmail.com>
2025-12-23 22:40:06 -0500

    Added bible con page

---
+++ javascript/conventionSignup.js
@@ -0,0 +1,5 @@
+document.getElementById('conventionSignupForm').addEventListener('submit', async (e) => {
+   
+    const msg = document.getElementById('signUpBtn');
+    msg.textContent = "Thank you!";
+});
